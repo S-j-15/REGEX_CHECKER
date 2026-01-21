cf9e09b02c5d80fc9f87ab0d0fe17d07f6d38cd7
S-j-15 <supratimjash@gmail.com>
2026-01-21 14:56:46 +0530

    Add files via upload

---
+++ regex_chk.py
@@ -0,0 +1,151 @@
+from collections import defaultdict
+from collections import deque
+def fix(reg):
+  r=list()
+  n=len(reg)
+  for i in range(n):
+    c=reg[i]
+    if c=="." or c=="(" or c=="|":
+      r.append(c)
+    elif (i+1<n) and reg[i+1]!=")" and reg[i+1]!="|" and reg[i+1]!="*":
+      r.append(c)
+      r.append(".")
+    else:
+      r.append(c)
+  return r
+      
+def pre_reg(reg):
+  s=list()
+  reg=fix(reg)
+  stack=list()
+  n=len(reg)
+  for i in range(n-1,-1,-1):
+    c=reg[i]
+    if c=="*" or c==")":
+      stack.append(c)
+    elif c==".":
+      while(len(stack) and stack[-1]=="*"):
+        r=stack.pop()
+        s=[r]+s
+      stack.append(c)
+    elif c=="|":
+      while(len(stack) and (stack[-1]=="*" or stack[-1]==".")):
+        r=stack.pop()
+        s=[r]+s
+      stack.append(c)
+    elif c=="(":
+      while stack[-1]!=")":
+        r=stack.pop()
+        s=[r]+s
+      stack.pop()
+      if len(stack) and stack[-1]=="*":
+        r=stack.pop()
+        s=[r]+s
+    else:
+      s=[c]+s
+  while stack:
+    s=[stack.pop()]+s
+  return s
+def make_NFA(reg):
+  reg=pre_reg(reg)
+  NFA=defaultdict(set)
+  stack=list()
+  state=0
+  n=len(reg)
+  for i in range(n-1,-1,-1):
+    c=reg[i]
+    if c=="|":
+      s1,leaf1=stack.pop()
+      s2,leaf2=stack.pop()
+      state+=1
+      NFA[(state,"eps")].add(s1)
+      NFA[(state,"eps")].add(s2)
+      leaf=leaf1.union(leaf2)
+      stack.append((state,leaf))
+    elif c==".":
+      s1,leaf1=stack.pop()
+      s2,leaf2=stack.pop()
+      for nd in leaf1:
+         NFA[(nd,"eps")].add(s2)
+      stack.append((s1,leaf2))
+    elif c=="*":
+      s1,leaf=stack.pop()
+      state+=1
+      s2=state
+      state+=1
+      s3=state    
+      for lfs in leaf:
+        NFA[(lfs,"eps")].add(s3)
+      state+=1
+      s4=state
+      NFA[(s3,"eps")].add(s4)
+      NFA[(s3,"eps")].add(s1)
+      NFA[(s2,"eps")].add(s1)
+      NFA[(s2,"eps")].add(s4)
+      stack.append((s2,{s4}))
+    else:
+      state+=1
+      s1=state
+      state+=1
+      s2=state
+      NFA[(s1,c)].add(s2)
+      stack.append((s1,{s2}))
+  return NFA,stack[-1]  
+#keep track of leaf
+#fix c=="." case in NFA
+#implement bfs based regex matcher
+def eval_NFA(NFA,start,end,str):
+  q=deque()
+  i=0
+  n=len(str)
+  q.append(start)
+  q.append(None)
+  nxt=set()
+  addit=0
+  allnxt=list()
+  while(len(q)>=1):
+    state=q.popleft()
+    if state==None:
+      i+=addit
+      if i>=n:
+        if len(nxt.intersection(end))!=0:
+          return True
+        else:
+          return False
+      if addit==0:
+        for ass in allnxt:
+          if ass==nxt:
+            return False
+        allnxt.append(nxt)
+      else:
+        allnxt=list()
+      nxt=set()
+      addit=0
+      q.append(None)
+    for st in NFA[(state,"eps")]:
+      if st not in nxt:
+        nxt.add(st)
+        q.append(st)
+    nl=len(NFA[(state,str[i])])
+    if nl>0:
+      addit=1
+    for st in NFA[(state,str[i])]:
+      if st not in nxt:
+        nxt.add(st)
+        q.append(st)
+  return False  
+    
+
+reg=input("ENTER REGEX:")
+print("-------------------")
+nfa,states=make_NFA(reg)
+print(fix(reg))
+print(pre_reg(reg))
+print(nfa)
+print(states)
+print("----------------------")
+while(True):
+  chk=input("ENTER STRING: ")
+  print(eval_NFA(nfa,states[0],states[1],chk))
+
+
